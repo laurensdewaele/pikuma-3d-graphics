218804ba0cbf1a0360c4ef8c9d0e5aec90356055
Laurens Dewaele <laurens.dewaele@gmail.com>
2024-06-26 11:50:22 +0200

    Cube points, orthographic projection

--- src/display.c
+++ src/display.c
@@ -48,7 +48,7 @@ void draw_grid(void) {
         for (int x = 0; x < window_width; x += 10) {
             color_buffer[(window_width * y) + x] = 0xFF444444;
         }
-    }
+}
 }
 
 void draw_pixel(int x, int y, uint32_t color) {
@@ -66,7 +66,7 @@ void draw_rect(int x, int y, int width, int height, uint32_t color) {
         for (int j = 0; j < height; j++) {
             int current_x = x + i;
             int current_y = y + j;
-            color_buffer[(window_width * current_y) + current_x] = color;
+            draw_pixel(current_x, current_y, color);
         }
     }
 }
--- src/main.c
+++ src/main.c
@@ -7,7 +7,33 @@
 
 bool is_running = false;
 
+const int N_POINTS = 9 * 9 * 9;
+vec3_t cube_points[N_POINTS];
+vec2_t cube_points_2D[N_POINTS];
+
+// Why 128?
+float fov_factor = 128;
+
+vec2_t project(vec3_t point) {
+    vec2_t point_2D = { fov_factor * point.x, fov_factor * point.y };
+    return point_2D;
+}
+
 void setup(void) {
+    // Create our cube
+    // So we need 
+    int point_count = 0;
+    for (float x = -1; x <= 1; x += .25) {
+        for (float y = -1; y <= 1; y += .25) {
+            for (float z = -1; z <= 1; z += .25) {
+                vec3_t new_point = { x , y , z };
+                cube_points[point_count] = new_point;
+                point_count += 1;
+            }
+        }
+    } 
+
+
     // Allocate the required memory in bytes to hold the color buffer
     color_buffer = (uint32_t*) malloc(sizeof(uint32_t) * window_width * window_height);
 
@@ -37,19 +63,26 @@ void process_input(void) {
 }
 
 void update(void) {
-    // TODO:
+    // Project our 3D cube points to 2D ones
+    for (int i = 0; i < N_POINTS; i++) {
+        vec3_t point = cube_points[i];
+        vec2_t point_2D = project(point);
+        cube_points_2D[i] = point_2D;
+    }
 }
 
 void render(void) {
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
-
-    vec3_t myvec = {3.0, 1.0, 2.2};
 
     draw_grid();
 
-    draw_pixel(50, 50, 0xFFFFFFFF);
-    draw_rect(300, 200, 300, 150, 0xFFFF00FF);
+    // Loop over 2D points and render them
+    for (int i = 0; i < N_POINTS; i++) {
+        vec2_t point = cube_points_2D[i];
+        draw_rect(
+            point.x + window_width / 2,
+            point.y + window_height / 2, 
+            4, 4, 0xFFFFFF00); 
+    }
 
     render_color_buffer();
     clear_color_buffer(0xFF000000);
